1fcb53c2e35900498995ee5d9872f7c4feb95a90
ion.drimba <ion.drimba@ibrasil.info>
2016-03-22 12:10:58 -0300

    First commit

---
+++ rename.js
@@ -0,0 +1,62 @@
+//node >  rename file version
+
+var fs = require('fs');
+var filePath = process.argv[2]; //SOURCE FILE PATH ex: Desktop/excel.xlsx;
+var version = process.argv[3]; //OUTPUT PATH ex: 1.2.3 - could be anything'
+var outputfolder = process.argv[4]; //OUTPUT PATH ex: Desktop/'
+
+// fs.rename('./teste.js', './teste-renamed.js', (err) => {
+//     if (err) throw err;
+//     console.log('renamed complete');
+// });
+// fs.stat('./teste-renamed.js', (err, stats) => {
+//     if (err) throw err;
+//     console.log(`stats: ${JSON.stringify(stats)}`);
+// });
+
+//file
+
+var fileName = removePathFromFileName(filePath),
+    fileExtension = getFileExtension(fileName),
+    tempNameNoExtension = getFileNameWithoutExtension(fileName),
+    fileRemamed = tempNameNoExtension + version + fileExtension; // hello. + version + .js
+
+
+function removePathFromFileName(input) {
+    var output = input;
+
+    //src/js/hello.js -> hello.js
+    output = input.replace(/\w+\//g, '');
+
+    return output;
+}
+
+function getFileExtension(input) {
+    var output = input;
+
+    //hello.js -> .js
+    output = input.replace(/[0-9a-z-0-1]{1,}/, '');
+
+    return output;
+}
+
+function getFileNameWithoutExtension(input) {
+    var output = input;
+
+    // hello.js -> hello.
+    output = input.replace(/[0-9a-z]+$/, '');
+
+    return output;
+}
+
+
+
+
+
+// var outputFile = output +
+
+// fs.createReadStream(file).pipe(fs.createWriteStream(outputFile));
+
+// fs.stat('./teste111-renamed.js', (err, stats) => {
+//     if (err) throw err;
+//     //console.log(`stats: ${JSON.stringify(stats)
