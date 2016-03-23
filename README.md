43e36c4a70f0db7bef35c981b556835d5ebfc0ec
Ion D. Filho <iondrimba@hotmail.com>
2016-03-23 17:40:59 -0300

    README update

--- README.md
+++ README.md
@@ -1,2 +1,95 @@
 # rename-me
-(not ready to use) Rename files with version tag to optimize cache
+Rename files with version tag to optimize cache
+
+
+####GOAL
+
+1. Avoid caching build files when they are modified.
+2. Avoid cache invalidation via query string ie: path/file.js?hash=#####
+3. Rename a file based on a version suplied as parameter and change its reference inside the html file.
+
+####INSTALL
+
+```sh
+ npm install rename-me --save-dev
+```
+####TODO
+
+* Write tests
+* Develop a Gulp plugin for easy integration
+
+####Usage via terminal
+
+Inside the terminal type:
+
+* keep the end / on the destination folder
+
+```sh
+ node rename-me file-path-to-rename.js version-number destination/ index.html
+```
+
+#####Real world:
+
+```sh
+ node .node_modules/rename-me/rename-me public/js/app.js 1.0.1 public/js/ public/index.html
+```
+
+#####Multiple files
+
+* multiple parameters are passed inside quotes and comma separeted 'param1', 'param2'
+
+```sh
+ node .node_modules/rename-me/rename-me 'public/js/app.js','public/css/app.css' 1.0.1 'public/js/','public/css/' public/index.html
+```
+
+####Using with Gulp (workaround)
+
+Install these dependencies
+```sh
+npm install gulp-bump --save-dev
+npm install semver --save-dev
+npm install rename-me --save-dev
+```
+Inside your gulpfile load them
+```js
+var bump = require('gulp-bump');
+var semver = require('semver');
+var renameMe = require('rename-me');
+```
+Read the package.json file and store it as an object
+```js
+var pckg = require('./package.json');
+```
+Bump the package.json version tag it using semver lib
+```js
+var patch = semver.inc(pckg.version, 'patch');
+var minor = semver.inc(pckg.version, 'minor');
+var major = semver.inc(pckg.version, 'major');
+```
+Define a function to bump the package.json version tag so we can use it
+```js
+function bumpPackageJson(type) {
+	return gulp.src(['./package.json'])
+		.pipe(bump({
+			version: type
+		}))
+		.pipe(gulp.dest('./'));
+}
+```
+Define the bump version tasks using bumpPackageJson
+```js
+//PATCH
+gulp.task('patch', function () {
+	return bumpPackageJson(patch);
+});
+
+//MINOR
+gulp.task('minor', function () {
+	return bumpPackageJson(minor);
+});
+
+//MAJOR
+gulp.task('major', function () {
+	return bumpPackageJson(major);
+});
+```
--- rename-me.js
+++ rename-me.js
@@ -1,6 +1,6 @@
 'use-strict';
 //node >  rename-me file version outputpath index.html
-//demo > node rename-me src/app.js 1.1.1 public/ public/index.html
+//demo > node rename-me src/app.js 1.1.1 public/js public/index.html
 //output > public/app.1.1.1.js
 
 var fs = require('fs');
@@ -9,14 +9,13 @@ var version = process.argv[3]; //OUTPUT PATH ex: 1.2.3 - could be anything'
 var outputfolder = process.argv[4]; //OUTPUT PATH ex: Desktop/'
 var indexFile = process.argv[5]; //INDEX FILE TO REPLACE REFERNCES OF OLD FILE'
 
-var fileName = [],
-    fileExtension = '',
-    tempNameNoExtension = '',
-    fileRenamed = '',
-    finalOutputPath = [];
+var fileNames = [],
+    fileExtensions = [],
+    tempNameNoExtension = [],
+    filesRenamed = [],
+    finalOutputPaths = [];
 
-
-var renameMe = function(options) {
+var renameMe = function (options) {
 
     filePath = options.filePath;
     version = options.version;
@@ -26,38 +25,39 @@ var renameMe = function(options) {
     try {
         setup();
         rename();
-        replaceReferences(fileRenamed);
+        replaceReferences(filesRenamed);
     } catch (ex) {
         console.log('ex:', ex.message);
     }
 
     function setup() {
-        fileName = removePathFromFileName(filePath);
-        fileExtension = getFileExtension(fileName);
-        tempNameNoExtension = getFileNameWithoutExtension(fileName);
+        fileNames = removePathFromFileName(filePath);
+        fileExtensions = getFileExtension(fileNames);
+        tempNameNoExtension = getFileNameWithoutExtension(fileNames);
 
-        fileRenamed = concatNamedVersion({
+        filesRenamed = concatNamedVersion({
             name: tempNameNoExtension,
             version: version,
-            extension: fileExtension
+            extension: fileExtensions
         });
 
-        outputfolder.forEach(function(folder, index) {
-            finalOutputPath.push(folder + fileRenamed[index]);
+        outputfolder.forEach(function (folder, index) {
+            finalOutputPaths.push(folder + filesRenamed[index]);
         });
     }
 
 
     function rename() {
-        filePath.forEach(function(path, index) {
+        //rename source files and move them to the destination folder
+        filePath.forEach(function (path, index) {
             fs.createReadStream(path)
-                .pipe(fs.createWriteStream(finalOutputPath[index]));
+                .pipe(fs.createWriteStream(finalOutputPaths[index]));
         });
     };
 
     function removePathFromFileName(files) {
         var output = [];
-        files.forEach(function(name) {
+        files.forEach(function (name) {
             var filename = name.replace(/.+([^\/])\//g, '');
             //src/js/hello.js -> hello.js
             output.push(filename);
@@ -67,7 +67,7 @@ var renameMe = function(options) {
 
     function getFileExtension(files) {
         var output = [];
-        files.forEach(function(name) {
+        files.forEach(function (name) {
             var filename = name.replace(/[0-9a-z-0-1]{1,}/, '');
 
             //hello.js -> .js
@@ -78,7 +78,7 @@ var renameMe = function(options) {
 
     function getFileNameWithoutExtension(files) {
         var output = [];
-        files.forEach(function(name) {
+        files.forEach(function (name) {
             var filename = name.replace(/[0-9a-z]+$/, '');
 
             // hello.js -> hello.
@@ -89,18 +89,22 @@ var renameMe = function(options) {
 
     function replaceReferences(newNames) {
         var contents = fs.readFileSync(indexFile, 'utf8');
-        var regex = new RegExp(fileName, "g");
+        var regex = new RegExp(fileNames, "g");
 
-        fs.readFile(indexFile, 'utf8', function(err, data) {
+        //read the main file (index.html)
+        fs.readFile(indexFile, 'utf8', function (err, data) {
             if (err) {
                 return console.log(err);
             }
-            fileName.forEach(function(name, index) {
+
+            //replace references inside the main file (index.html)
+            fileNames.forEach(function (name, index) {
                 regex = new RegExp(name, "g");
-                contents = contents.replace(regex, newNames[index]);              
+                contents = contents.replace(regex, newNames[index]);
             });
 
-            fs.writeFile(indexFile, contents, 'utf8', function(err) {
+            //write the main file (index.html) with the new references
+            fs.writeFile(indexFile, contents, 'utf8', function (err) {
                 if (err) return console.log(err);
             });
         });
@@ -108,19 +112,19 @@ var renameMe = function(options) {
 
     function concatNamedVersion(options) {
         var output = [];
-        options.name.forEach(function(name, index) {
+        options.name.forEach(function (name, index) {
             var filename = name.replace(/[0-9a-z]+$/, '');
 
-            // hello.js -> hello.version.js            
+            // hello.js -> hello.version.js (hello.1.2.0.js)
             output.push(options.name[index] + options.version + options.extension[index]);
         });
         return output;
     };
 }
 
-//execute if called manually via terminal
+//executed if called manually via terminal
 if (process.argv.length > 3) {
-    var options = {};    
+    var options = {};
     options.filePath = filePath.split(',');;
     options.version = version;
     options.outputfolder = outputfolder.split(',');;
