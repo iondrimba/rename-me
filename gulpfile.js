fe8f2052fef2af8301f1860d45e2904153385f92
Ion D. FIlho <iondrimba@hotmail.com>
2016-03-22 23:49:21 -0300

    Renaming multiple files

--- gulpfile.js
+++ gulpfile.js
@@ -3,25 +3,18 @@ var bump = require('gulp-bump');
 
 var fs = require('fs');
 var semver = require('semver');
+var pckg = require('./package.json');
 
-var getPackageJson = function () {
-  return JSON.parse(fs.readFileSync('./package.json', 'utf8'));
-};
 
-// bump versions on package/bower/manifest
-gulp.task('bump', function () {
-  // reget package
-  var pkg = getPackageJson();
-  // increment version
-  var newVer = semver.inc(pkg.version, 'patch');
-  return gulp.src(['./package.json'])
-    .pipe(bump({
-      version: newVer
-    }))
-    .pipe(gulp.dest('./'));
+gulp.task('bump', function() {
+    var newVer = semver.inc(pckg.version, 'patch');
+    return gulp.src(['./package.json'])
+        .pipe(bump({
+            version: newVer
+        }))
+        .pipe(gulp.dest('./'));
 });
 
-// Run the gulp tasks
-gulp.task('default', function(){
-  gulp.run('bump');
+gulp.task('default', function() {
+    gulp.run('bump');
 });
--- rename-me.js
+++ rename-me.js
@@ -8,20 +8,19 @@ var filePath = process.argv[2]; //SOURCE FILE PATH ex: Desktop/excel.xlsx;
 var version = process.argv[3]; //OUTPUT PATH ex: 1.2.3 - could be anything'
 var outputfolder = process.argv[4]; //OUTPUT PATH ex: Desktop/'
 var indexFile = process.argv[5]; //INDEX FILE TO REPLACE REFERNCES OF OLD FILE'
-var semver = require('semver');
 
-var fileName = '',
+var fileName = [],
     fileExtension = '',
     tempNameNoExtension = '',
     fileRenamed = '',
-    finalOutputPath = '';
+    finalOutputPath = [];
 
 
-var renameMe = function (options) {
+var renameMe = function(options) {
 
-    filePath = options.filePath;
-    version = semver.inc(options.version, 'patch');
-    outputfolder = options.outputfolder;
+    filePath = options.filePath.split(',');
+    version = options.version;
+    outputfolder = options.outputfolder.split(',');
     indexFile = options.indexFile;
 
     try {
@@ -36,75 +35,91 @@ var renameMe = function (options) {
         fileName = removePathFromFileName(filePath);
         fileExtension = getFileExtension(fileName);
         tempNameNoExtension = getFileNameWithoutExtension(fileName);
+
         fileRenamed = concatNamedVersion({
             name: tempNameNoExtension,
             version: version,
             extension: fileExtension
         });
-        finalOutputPath = outputfolder + fileRenamed;
+
+        outputfolder.forEach(function(folder, index) {
+            finalOutputPath.push(folder + fileRenamed[index]);
+        });
     }
 
 
     function rename() {
-
-        fs.createReadStream(filePath)
-            .pipe(fs.createWriteStream(finalOutputPath));
+        filePath.forEach(function(path, index) {
+            fs.createReadStream(path)
+                .pipe(fs.createWriteStream(finalOutputPath[index]));
+        });
     };
 
-    function removePathFromFileName(input) {
-        var output = input;
-
-        //src/js/hello.js -> hello.js
-        output = input.replace(/.+([^\/])\//g, '');
-
+    function removePathFromFileName(files) {
+        var output = [];
+        files.forEach(function(name) {
+            var filename = name.replace(/.+([^\/])\//g, '');
+            //src/js/hello.js -> hello.js
+            output.push(filename);
+        });
         return output;
     };
 
-    function getFileExtension(input) {
-        var output = input;
+    function getFileExtension(files) {
+        var output = [];
+        files.forEach(function(name) {
+            var filename = name.replace(/[0-9a-z-0-1]{1,}/, '');
 
-        //hello.js -> .js
-        output = input.replace(/[0-9a-z-0-1]{1,}/, '');
-
+            //hello.js -> .js
+            output.push(filename);
+        });
         return output;
     };
 
-    function getFileNameWithoutExtension(input) {
-        var output = input;
+    function getFileNameWithoutExtension(files) {
+        var output = [];
+        files.forEach(function(name) {
+            var filename = name.replace(/[0-9a-z]+$/, '');
 
-        // hello.js -> hello.
-        output = input.replace(/[0-9a-z]+$/, '');
-
+            // hello.js -> hello.
+            output.push(filename);
+        });
         return output;
     };
 
-    function replaceReferences(newName) {
+    function replaceReferences(newNames) {
         var contents = fs.readFileSync(indexFile, 'utf8');
         var regex = new RegExp(fileName, "g");
-        var output = '';
 
-        fs.readFile(indexFile, 'utf8', function (err, data) {
+        fs.readFile(indexFile, 'utf8', function(err, data) {
             if (err) {
                 return console.log(err);
             }
-            output = contents.replace(regex, newName);
-            fs.writeFile(indexFile, output, 'utf8', function (err) {
+            fileName.forEach(function(name, index) {
+                regex = new RegExp(name, "g");
+                contents = contents.replace(regex, newNames[index]);              
+            });
+
+            fs.writeFile(indexFile, contents, 'utf8', function(err) {
                 if (err) return console.log(err);
             });
         });
     };
 
     function concatNamedVersion(options) {
-        var output = '';
+        var output = [];
+        options.name.forEach(function(name, index) {
+            var filename = name.replace(/[0-9a-z]+$/, '');
 
-        output = options.name + options.version + options.extension;
-
+            // hello.js -> hello.version.js            
+            output.push(options.name[index] + options.version + options.extension[index]);
+        });
         return output;
     };
 }
 
 //execute if called manually via terminal
-if (process.argv[4]) {
+if (process.argv[2]) {
     var options = {};
     options.filePath = filePath;
     options.version = version;
